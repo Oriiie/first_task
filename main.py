e43eef1dc0299f98d47892f83a94cf07de076c89
Oriiie <46246359+Oriiie@users.noreply.github.com>
2022-07-11 19:07:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+def equals(first: str, second: str) -> bool:
+    left = [i for i in first]
+    right = [i for i in second]
+    left.sort()
+    right.sort()
+    return left == right
+
+
+class TestSum(unittest.TestCase):
+
+    def test_usual(self):
+        self.assertEqual(equals('hello', 'olleh'), True, "Палиндром")
+
+    def test_capital_letter(self):
+        self.assertEqual(equals('apple', 'Apple'), False, "Не палиндром")
+
+    def test_spaces(self):
+        self.assertEqual(equals('hello friend', 'hellofriend'), False, "Не палиндром")
+
+    def test_fullrandom(self):
+        self.assertEqual(equals('abcdqwer', 'qdwcebra'), True, "Не палиндром")
+
+
+unittest.main()
+
